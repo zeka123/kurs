d2aed39a129794120ec6a2d363dc5679f7589312
zeka123 <bobrovevgeniy2000@mail.ru>
2020-04-15 17:54:59 +0300

    Add files via upload

--- add.php
+++ add.php
@@ -4,7 +4,7 @@
 <?php
 $pass=$_POST['password'];
 $login=$_POST['login'];
-$mysql=new mysqli('localhost','root','','turn');
+$mysql=new mysqli('95.217.23.55','root@localhost','root','turn');
 
 if ($mysql->connect_error) {
     die("Connection error: ".$mysql>connect_error);
